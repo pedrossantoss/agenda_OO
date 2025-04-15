6c0f7b58b992a36c9567dce15f0ba64c13119977
Rodrigoatila09 <154346800+Rodrigoatila09@users.noreply.github.com>
2025-04-14 23:45:08 -0300

    Ajustes no arquivo

--- Agenda_atilaa/src/Contato.py
+++ Agenda_atilaa/src/Contato.py
@@ -1,11 +1,12 @@
 class Contacts():
     # arrayContcts = []
 
-    def __init__(self,name, id, saveID):
+    def __init__(self,name, id, saveID, editFunction):
         self.name = name
         self.idContact = id
         self.saveID = saveID
         self.arrayContacts = []
+        self.edit = editFunction
 
     def createContact(self):
         self.idContact += 1
@@ -20,26 +21,41 @@ class Contacts():
         for contact in self.arrayContacts:
             print(contact)
 
-    def logicDeleteContacts(self):
-        find_ID = False
+    def deleteContacts(self):
+        status = False
         input_delete = int(input("Digite o ID do contato que deseja deletar:\n"))
         for contact_ID in self.arrayContacts:
             if(input_delete == contact_ID["id"]):
                 self.arrayContacts.remove(contact_ID)
-                find_ID = True
-                if(find_ID == True):
+                status = True
+                if(status == True):
                     print("Contato excluído com sucesso!")
             
-        if(find_ID == False):
+        if(status == False):
                 print("ID não identificado, tente novamente.")
         
     
-    
-    
-                
-    
+    def editContact(self):
+        saveEdit = int(input("Digite o ID de quem deseja editar:\n"))
+        status = False
+        for edit_ID in self.arrayContacts:
+            if(saveEdit == edit_ID["id"]):
+                newName = input("Insira o novo nome:\n")
+                edit_ID["Nome"] = newName
+                self.edit = edit_ID
+                status = True
+                if(status == True):
+                    print("Usuário editado com sucesso!")
+                break
+        if(status == False):
+            print("Erro ao editar o usuário") 
 
-instanciarNome = Contacts("", 0,"")
-instanciarNome.createContact()
-instanciarNome.readContacts()
-instanciarNome.logicDeleteContacts()
+
+
+instanciarNome = Contacts("", 0,"","")
+# instanciarNome.createContact()
+# instanciarNome.readContacts()
+# instanciarNome.editContact()
+# instanciarNome.readContacts()
+# instanciarNome.deleteContacts()
+# instanciarNome.readContacts()
